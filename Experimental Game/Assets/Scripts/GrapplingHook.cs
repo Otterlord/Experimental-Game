eb1f9dd241fda00bff6a07b1d1ef3942c5303ce3
Otterlord <yourotterlord@gmail.com>
2019-04-19 14:41:47 -0700

    grappling hooks

---
+++ Experimental Game/Assets/Scripts/GrapplingHook.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrapplingHook : MonoBehaviour
+{
+    // Physics tweaks
+    public float maxDistance;
+    public float travelSpeed;
+
+    public LayerMask grappleStuff;
+
+    // Other stuff
+    private Vector3 target;
+    private bool going = false;
+
+    // References
+    private Player player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GetComponent<Player>();   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, maxDistance, grappleStuff);
+            target = hit.point;
+            player.enabled = false; // stop player from doing thing
+            going = true;
+        }
+        if (going)
+        {
+            if (transform.position == target)
+            {
+                player.enabled = true;
+                going = false;
+                return;
+            }
+
+            transform.Translate((target - transform.position).normalized * travelSpeed * Time.deltaTime);
+        }
+    }
+}
